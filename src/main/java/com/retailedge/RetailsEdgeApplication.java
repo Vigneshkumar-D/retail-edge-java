2832834c88d195cebf43e187796cbd42fe5fe12a
Vigneshkumar D <126967042+Vigneshkumar-D@users.noreply.github.com>
2024-12-09 15:35:16 +0530

    Update RetailsEdgeApplication.java

--- src/main/java/com/retailedge/RetailsEdgeApplication.java
+++ src/main/java/com/retailedge/RetailsEdgeApplication.java
@@ -9,10 +9,23 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+// @SpringBootApplication
+// @EnableAutoConfiguration
+// @EnableScheduling
+// //@EnableJpaAuditing
+// @EnableAsync
+// public class RetailsEdgeApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(RetailsEdgeApplication.class, args);
+//         System.out.println("Application is Running....");
+//     }
+// }
+
+
 @SpringBootApplication
-@EnableAutoConfiguration
 @EnableScheduling
-//@EnableJpaAuditing
+// @EnableJpaAuditing // Uncomment if auditing is required later
 @EnableAsync
 public class RetailsEdgeApplication {
 
@@ -21,4 +34,5 @@ public class RetailsEdgeApplication {
         System.out.println("Application is Running....");
     }
 }
+
 
